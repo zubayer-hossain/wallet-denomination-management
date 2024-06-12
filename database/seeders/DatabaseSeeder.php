630fd1b58c7f27bd140c645bf57c2cf0b9e98b69
Zubayer Hossain <zubayerh4@gmail.com>
2024-06-12 13:32:51 +0600

    Added default user in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(CurrencySeeder::class);
-
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(CurrencySeeder::class);
+
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => Hash::make('password'),
+         ]);
     }
 }
