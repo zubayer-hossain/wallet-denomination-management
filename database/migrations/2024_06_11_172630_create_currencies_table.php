66c6704416248a555c44871ad1eec00d02e91fb1
Zubayer Hossain <zubayerh4@gmail.com>
2024-06-12 13:08:18 +0600

    Create migration and seeder for currency

---
+++ database/migrations/2024_06_11_172630_create_currencies_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('symbol');
+            $table->string('icon')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Define the currencies to seed
+        $currencies = [
+            ['name' => 'U.S. Dollar', 'symbol' => 'USD', 'icon' => '$'],
+            ['name' => 'Bangladeshi Taka', 'symbol' => 'BDT', 'icon' => '৳'],
+            ['name' => 'Malaysian Ringgit', 'symbol' => 'MYR', 'icon' => 'RM']
+        ];
+
+        // Insert currencies into the database
+        foreach ($currencies as $currency) {
+            DB::table('currencies')->insert([
+                'name' => $currency['name'],
+                'symbol' => $currency['symbol'],
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
+
